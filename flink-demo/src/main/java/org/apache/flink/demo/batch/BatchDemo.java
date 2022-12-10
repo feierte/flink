260cbd71959f3caaf242e881b0d3d54866d12a86
zhaojie <1979170837@qq.com>
2022-12-10 22:35:53 +0800

    initial demo

---
+++ flink-demo/src/main/java/org/apache/flink/demo/batch/BatchDemo.java
@@ -0,0 +1,16 @@
+package org.apache.flink.demo.batch;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+/**
+ * @author jie zhao
+ * @date 2022/12/10 13:19
+ */
+public class BatchDemo {
+
+    public static void main(String[] args) {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
+}
---
+++ flink-demo/src/main/java/org/apache/flink/demo/domain/AccessLog.java
@@ -0,0 +1,36 @@
+package org.apache.flink.demo.domain;
+
+import lombok.Data;
+
+import org.apache.flink.demo.util.IpUtils;
+
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Random;
+
+/**
+ * @author jie zhao
+ * @date 2022/12/10 12:19
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccessLog {
+    
+    private long apiId;
+    private String sip;
+    private String dip;
+
+    private static Random random = new Random();
+
+    public AccessLog(long apiId, String sip, String dip) {
+        this.apiId = apiId;
+        this.sip = sip;
+        this.dip = dip;
+    }
+
+    public static AccessLog newInstance() {
+        return new AccessLog(random.nextLong(), IpUtils.getRandomIp(), IpUtils.getRandomIp());
+    }
+}
