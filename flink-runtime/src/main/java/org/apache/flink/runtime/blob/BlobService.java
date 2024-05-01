78a419af7cd747ea2f69b04efa701b564661beec
HW <1979170837@qq.com>
2024-05-01 22:34:43 +0800

    BlobService

--- flink-runtime/src/main/java/org/apache/flink/runtime/blob/BlobService.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/blob/BlobService.java
@@ -20,7 +20,14 @@ package org.apache.flink.runtime.blob;
 
 import java.io.Closeable;
 
-/** A simple store and retrieve binary large objects (BLOBs). */
+/**
+ * A simple store and retrieve binary large objects (BLOBs).
+ *
+ * @apiNote BlobService 是用于管理和存储 Flink 作业所需的各种资源文件的服务。这些资源文件可以是用户上传的 JAR 文件、类库、配置文件等。
+ * BlobService 是 C/S 模式，BlobServer 是服务端，负责接收客户端的上传 Blob 请求，并根据请求将 Blob 存储到相应的 BlobStore 中。
+ * BlobServer 还负责处理客户端下载 Blob 的请求。BlobClient 是客户端，用于上传和下载 Blob，BlobClient 向 BlobServer 发送上传请求，
+ * 并根据 BlobKey 下载 Blob。BlobClient 和 BlobServer 之间的通信采用的是 BIO Socket。
+ */
 public interface BlobService extends Closeable {
 
     /**
